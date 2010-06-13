145eed30cd211bfa7c94bd786c054f68cb66f40f
Matthew Todd <matthew.todd@gmail.com>
2010-06-13 17:13:11 +0300

    There's just 1 jar now.

--- lib/settlers/java.rb
+++ lib/settlers/java.rb
@@ -1,21 +1,20 @@
 module Settlers
   class Java
     def self.server
-      new('JSettlers.jar', 'soc.server.SOCServer')
+      new('soc.server.SOCServer')
     end
 
     def self.robot
-      new('JSettlers.jar', 'soc.robot.SOCRobotClient')
+      new('soc.robot.SOCRobotClient')
     end
 
     def self.client
-      new('JSettlers.jar', 'soc.client.SOCPlayerClient')
+      new('soc.client.SOCPlayerClient')
     end
 
-    PATH = Settlers.datadir.join('jsettlers-1.1.09')
+    JAR = Settlers.datadir.join('jsettlers-1.1.09', 'JSettlers.jar')
 
-    def initialize(jar_name, class_name)
-      @jar_name   = jar_name
+    def initialize(class_name)
       @class_name = class_name
     end
 
@@ -31,7 +30,7 @@ module Settlers
     private
 
     def command(args)
-      ['java', '-cp', PATH.join(@jar_name), @class_name].concat(args.map { |arg| arg.to_s })
+      ['java', '-cp', JAR, @class_name].concat(args.map { |arg| arg.to_s })
     end
   end
 end
