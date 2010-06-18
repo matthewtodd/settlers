d873975cdba186eab79ef1f07c1a79fc70d12688
Matthew Todd <matthew.todd@gmail.com>
2010-06-18 11:33:21 +0300

    Tweak readme usage.

--- lib/settlers/application.rb
+++ lib/settlers/application.rb
@@ -13,13 +13,13 @@ module Settlers
         opts.separator ''
         opts.version = Settlers::VERSION
 
-        opts.on('-c', '--client', 'Discover and connect to a server.') do
+        opts.on('-c', '--client', 'Discover and connect to a game server.') do
           @announcer  = NullObject.new
           @discoverer = Bonjour.new
           @server     = NullObject.new
         end
 
-        opts.on('-s', '--server[=PORT]', Integer, 'Launch, announce, and connect to a server.', ". PORT defaults to #{DEFAULT_PORT}.") do |port|
+        opts.on('-s', '--server[=PORT]', Integer, 'Launch, announce, and connect to a game server.', ". PORT defaults to #{DEFAULT_PORT}.") do |port|
           @announcer  = Bonjour.new
           @discoverer = NullObject.new
           @server     = Server.new(port || DEFAULT_PORT)
