a31741ca59a5f95da4794495b75090fb673c39ca
Matthew Todd <matthew.todd@gmail.com>
2010-06-18 11:15:11 +0300

    Build up CLI for different game types.

--- lib/settlers/application.rb
+++ lib/settlers/application.rb
@@ -1,34 +1,61 @@
 module Settlers
   class Application
-    PORT = 8880
+    DEFAULT_PORT = 7777
 
     def initialize
-      @bonjour   = Bonjour.new
       @collector = Collector.new
       @ui        = UI.new
 
+      @defaults = %w(--server)
+
       @options = OptionParser.new do |opts|
+        opts.banner  = "Usage: #{File.basename($0)} [--client]"
+        opts.separator ''
         opts.version = Settlers::VERSION
 
-        opts.on('-V', '--verbose') do
-          @ui = UI::Verbose.new
+        opts.on('-c', '--client', 'Discover and connect to a server.') do
+          @announcer  = NullObject.new
+          @discoverer = Bonjour.new
+          @server     = NullObject.new
         end
+
+        opts.on('-s', '--server[=PORT]', Integer, 'Launch, announce, and connect to a server.', "  (PORT defaults to #{DEFAULT_PORT}.)") do |port|
+          @announcer  = Bonjour.new
+          @discoverer = NullObject.new
+          @server     = Server.new(port || DEFAULT_PORT)
+        end
+
+        opts.separator ''
+        opts.separator 'Defaults:'
+        opts.separator "#{opts.summary_indent}#{@defaults.join(' ')}"
       end
     end
 
     def run(args)
-      @options.parse(args)
+      @options.parse!(@defaults.dup.concat(args))
 
-      server = Server.new(PORT)
-      server.add_observer(@bonjour)
-      server.add_observer(@collector)
-      server.start
+      @server.add_observer(@announcer)
+      @server.add_observer(@collector)
+      @server.start
 
-      @bonjour.add_observer(@collector)
-      @bonjour.start
+      @discoverer.add_observer(@collector)
+      @discoverer.start
 
       @ui.choose_server(@collector) do |address|
         Client.new(address).start
+      end
+    end
+
+    private
+
+    class NullObject
+      def add_observer(observer)
+      end
+
+      def update(*args)
+      end
+
+      def start
       end
     end
 
